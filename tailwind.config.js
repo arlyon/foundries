a400cf51cb1af6df45c401230bafcd323ade350f
Alexander Lyon <arlyon@me.com>
2022-05-19 10:02:35 +0100

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx"],
   // https://github.com/tailwindlabs/tailwindcss-forms
   plugins: [require("@tailwindcss/forms")],
   theme: {
     fontFamily: {
       sans: ["Space Grotesk"],
     },
-  }
+  },
 };
